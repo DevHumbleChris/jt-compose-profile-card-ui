5c139d49543b0ccc5c8befdfd0b9b695828208a4
DevHumbleChris <dev.humblechris254@gmail.com>
2024-02-03 08:15:03 +0300

    implemented HorizontalPager

--- app/src/main/java/com/example/profilecardui/app/ProfileCardUI.kt
+++ app/src/main/java/com/example/profilecardui/app/ProfileCardUI.kt
@@ -1,5 +1,6 @@
 package com.example.profilecardui.app
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -8,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.example.profilecardui.screens.MainNavigation
 
@@ -20,12 +22,13 @@ fun ProfileCardUIApp() {
             TopAppBar(
                 title = { 
                     Text(text = "Friendzy")
-                }
+                },
             )
         }
     ) {
         Surface(
-            modifier = Modifier.padding(it)
+            modifier = Modifier.padding(it).fillMaxSize(),
+            color = Color.White
         ) {
             MainNavigation(navController)
         }
--- app/src/main/java/com/example/profilecardui/screens/HomeProfiles.kt
+++ app/src/main/java/com/example/profilecardui/screens/HomeProfiles.kt
@@ -1,9 +1,35 @@
 package com.example.profilecardui.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import com.example.profilecardui.R
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun HomeProfiles(navController: NavHostController) {
+    val pics = listOf(
+        R.drawable.woman,
+        R.drawable.woman,
+        R.drawable.woman
+    )
 
+    val pagerState = rememberPagerState(
+        initialPage = 0,
+        initialPageOffsetFraction = 0f,
+        pageCount = {
+            pics.size
+        }
+    )
+
+    Column {
+        HorizontalPager(state = pagerState) { index ->
+            Image(painter = painterResource(id = pics[index]), contentDescription = null)
+        }
+    }
 }
